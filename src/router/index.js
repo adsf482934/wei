e67f50fa79c579000d8f97dfc9b8bb705ed8ec06
wei <932054275@qq.com>
2019-02-17 22:03:07 +0800

    登录-引入表单组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/login.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  routes: [ {
+    name: 'login',
+    path: '/login',
+    component: Login
+  }]
 })
